08b93d96183b147665495545617a49a6c9031083
Rasul Osmanov <maccarty845@gmail.com>
2022-04-08 01:03:33 +0300

    08.04

---
+++ Checks/Check.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using Cinema;
+
+namespace Cinema.Checks
+{
+    public class Check
+    {
+        DataBase dataBase = new DataBase();
+        public Boolean User(string login, string password)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string queryString = $"SELECT * FROM Register WHERE LOGIN = '{login}' AND PASSWORD = '{password}'";
+
+            SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Аккаунт уже существует:", "Аккаунт существует", MessageBoxButton.OK, MessageBoxImage.Information);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean Discipline(string name)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string queryString = $"SELECT * FROM Disciplines WHERE DisciplineName = '{name}'";
+
+            SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Дисциплина уже существует:", "Дисциплина существует", MessageBoxButton.OK, MessageBoxImage.Information);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean Group(string name)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string queryString = $"SELECT * FROM Groups WHERE GroupName = '{name}'";
+
+            SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Группа уже существует:", "Группа существует", MessageBoxButton.OK, MessageBoxImage.Information);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean Student(string firstName, string lastName, string middleName)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string queryString = $"SELECT * FROM Students WHERE StudentFirstName = '{firstName}' AND StudentLastName = '{lastName}' AND StudentMiddleName = '{middleName}'";
+
+            SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Студент уже существует:", "Студент существует", MessageBoxButton.OK, MessageBoxImage.Information);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string SearchElementID(string SearchRow)
+        {
+            string SearchElementId = null;
+            for (int i = 0; i < SearchRow.Length; i++)
+            {
+                if (SearchRow[i] == ':')
+                {
+                    break;
+                }
+                else
+                {
+                    SearchElementId += SearchRow[i];
+                }
+            }
+            return SearchElementId;
+        }
+
+        public string WhatAction(int ActionId)
+        {
+            string ActionName="НЕсработало";
+            switch (ActionId)
+            {
+                case 0:
+                    ActionName="Добавление";
+                    break;
+                case 1:
+                    ActionName = "Редактирование";
+                    break;
+                case 2:
+                    ActionName = "Удаление";
+                    break;
+            }
+            return ActionName;
+        }
+    }
+}
--- SaleTicket.xaml.cs
+++ SaleTicket.xaml.cs
@@ -12,16 +12,16 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using WPF_StudentsAchievement_Project.Resources.Profiles;
+using Cinema.Checks;
+using Cinema;
 
 namespace Cinema
 {
-    /// <summary>
-    /// Логика взаимодействия для SaleTicket.xaml
-    /// </summary>
     public partial class SaleTicket : Window
     {
+        Check check = new Check();
         DataBaseField baseField = new DataBaseField();
+        //DataBaseField baseField = new DataBaseField();
         SqlConnection sqlConnection = new SqlConnection("Data Source=MSI-AEGIS-TI3;Initial Catalog=Cinema;Integrated Security=True");
         DataBase dataBase = new DataBase();
 
@@ -38,27 +38,64 @@ namespace Cinema
         public void FilmComboBoxRefresh()
         {
             dataBase.OpenConnection();
-            string queryString = "SELECT film FROM film";
+            string queryString = "SELECT id_film,name FROM film";
             SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
             SqlDataReader sqlDataReader = command.ExecuteReader();
             FilmComboBox.Items.Clear();
             while (sqlDataReader.Read())
             {
-                string film = sqlDataReader.GetString(1);
-                FilmComboBox.Items.Add($"{film}");
+                int id = sqlDataReader.GetInt32(0);
+                string filmName = sqlDataReader.GetString(1);
+                FilmComboBox.Items.Add($"{id}:{filmName}");
             }
             dataBase.CloseConnection();
+        }
+        public void HallComboBoxRefresh()
+        {
+            dataBase.OpenConnection();
+            string queryString = "SELECT id_hall,name FROM hall";
+            SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+            SqlDataReader sqlDataReader = command.ExecuteReader();
+            HallComboBox.Items.Clear();
+            while (sqlDataReader.Read())
+            {
+                int id = sqlDataReader.GetInt32(0);
+                string hall = sqlDataReader.GetString(1);
+                HallComboBox.Items.Add($"{id}:{hall}");
+            }
+            dataBase.CloseConnection();
+        }
+        //public void DateTimeComboBoxRefresh()
+        //{
+        //    dataBase.OpenConnection();
+        //    string queryString = "SELECT id_film,date FROM film";
+        //    SqlCommand command = new SqlCommand(queryString, dataBase.GetConnection());
+        //    SqlDataReader sqlDataReader = command.ExecuteReader();
+        //    DateTimeComboBox.Items.Clear();
+        //    while (sqlDataReader.Read())
+        //    {
+        //        int id = sqlDataReader.GetInt32(0);
+        //        int date = sqlDataReader.GetInt32(1);
+        //        DateTimeComboBox.Items.Add($"{id}:{date}");
+        //    }
+        //    dataBase.CloseConnection();
+        //}
+
+        private void FilmComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            baseField.Film = (check.SearchElementID(FilmComboBox.Text));
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             FilmComboBoxRefresh();
+            HallComboBoxRefresh();
+            //DateTimeComboBoxRefresh();
         }
-
-        //private void ComboBox_DropDownClosed(object sender, EventArgs e)
-        //{
-        //    baseField.Film = Convert.ToInt32(check.SearchElementID(FilmComboBox.Text));
-        //}
-
     }
 }
